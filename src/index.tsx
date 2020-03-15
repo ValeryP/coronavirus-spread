c0ced6f1553e1a773dda1355bb1ab6e1c43df603
p_val <palval16@gmail.com>
2020-03-15 17:39:43 +0100

    Removed search params for page views GA tracking

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactGA from 'react-ga';
 import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
 
 ReactGA.initialize("UA-160685541-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.pageview(window.location.pathname);
 
 const theme = createMuiTheme();
 ReactDOM.render(<ThemeProvider theme={theme}><App/></ThemeProvider>,
