5b9a4c27fe5c0caabbc7da47bb1153f03f0c0251
p_val <palval16@gmail.com>
2020-03-18 08:12:37 +0100

    Updates

---
+++ precache-manifest.7965319a46948810cfabb66d2152883d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15082c1f3558e838a97d89c14a1c27e8",
+    "url": "/coronavirus-spread/index.html"
+  },
+  {
+    "revision": "104646f6ee06d0e8d62c",
+    "url": "/coronavirus-spread/static/js/2.35cc1973.chunk.js"
+  },
+  {
+    "revision": "71ce35d6338fc27725434925720d0cab",
+    "url": "/coronavirus-spread/static/js/2.35cc1973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fbe645d5237079b4a1",
+    "url": "/coronavirus-spread/static/js/main.2b9ae56f.chunk.js"
+  },
+  {
+    "revision": "5dfa480bd9a1febfe923",
+    "url": "/coronavirus-spread/static/js/runtime-main.fe16824a.js"
+  }
+]);
