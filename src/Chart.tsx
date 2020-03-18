e6779ee10085d7ff0af8d9b9a893faaddcce8848
p_val <palval16@gmail.com>
2020-03-18 22:14:58 +0100

    Prediction works for +1d [not finished]

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Line} from 'react-chartjs-2';
-import _ from "lodash";
+import _, {Dictionary} from "lodash";
 import moment from "moment";
 
 export default function Chart({labels, dataPerCountry, country, days, color}: { labels: Date[], dataPerCountry: string[], country: string, days: number, color: string }) {
@@ -25,102 +25,78 @@ export default function Chart({labels, dataPerCountry, country, days, color}: { 
     const predictionLineStyle = {
         ...commonLineStyle,
         borderDash: [5, 2],
-        pointRadius: 5,
+        pointRadius: 2,
         backgroundColor: '#000AA',
         borderColor: '#2196f3',
+        pointHoverRadius: 5,
         pointBorderColor: '#2196f3',
         pointHoverBackgroundColor: '#2196f3',
         pointHoverBorderColor: '#00033'
     };
 
-    function normailizeLatestDaysNumber(latestDaysNumber: number) {
-        let latestDaysCases = _.slice(dataPerCountry, dataPerCountry.length - latestDaysNumber - 1);
-        const latestDaysCasesNonZero = latestDaysCases.filter(x => x !== "0").length
-        return latestDaysCasesNonZero < latestDaysNumber ? latestDaysCasesNonZero - 1 : latestDaysNumber;
-    }
 
-    function buildNextDaysLabels(startingDay: Date | undefined, days: number): Date[] {
+    function buildNextDaysLabels(startingDay: Date, days: number): Date[] {
         return _.reduce(_.range(1, +days + +1), (acc, v, i) => {
             acc[i] = moment(startingDay || Date.now()).add(i + 1, "days").toDate();
             return acc
         }, [] as Date[]);
     }
 
-    function buildPrediction(dataArray: number[], timeRange: number, predictionSample: number, logs: boolean) {
-        const latestDaysNumber = normailizeLatestDaysNumber(predictionSample)
-        const latestDaysCases = _.takeRight(dataArray, latestDaysNumber);
-        const latestDaysDelta = latestDaysCases.map((v, i) => i === 0 ? 0 : v - latestDaysCases[i - 1])
-        const mults = _.reduce(latestDaysCases, (acc, value, i) => {
-            let previousValue = latestDaysCases[i - 1];
+    function buildPrediction(dataArray: number[], logs: boolean = false) {
+        const mults = _.reduce(dataArray, (acc, value, i) => {
+            let previousValue = dataArray[i - 1];
             if (i > 0 && previousValue > 0 && !isNaN(value) && !isNaN(previousValue)) {
                 acc[i - 1] = value / previousValue
             }
             return acc
         }, [] as number[])
-        let remove = _.filter(mults, x => !_.isNaN(x) && x !== 1);
+        let remove = _.filter(mults, x => !_.isNaN(x));
         const avgMult = _.mean(remove) || 1
         if (logs) {
-            // console.warn('xxx - latestDaysNumber:', latestDaysNumber)
-            // console.warn('xxx - latestDaysCases:', latestDaysCases)
-            // console.warn('xxx - latestDaysDelta:', latestDaysDelta)
-            // console.warn('xxx - mults:', mults)
-            // console.warn('xxx - remove:', remove)
-            // console.warn('xxx - avgMult:', avgMult)
+            console.warn('xxx - mults:', mults)
+            console.warn('xxx - remove:', remove)
+            console.warn('xxx - avgMult:', avgMult)
         }
-        return _.reduce(_.range(1, +timeRange + +1), (acc, v, i) => {
-            acc[i] = Math.round(_.reduce(_.range(0, v), (acc) => acc * avgMult, 1) * (_.last(latestDaysCases) || 0));
+        return _.reduce(_.range(1, 2), (acc, v, i) => {
+            acc[i] = Math.round(_.reduce(_.range(0, v), (acc) => acc * avgMult, 1) * (_.last(dataArray) || 0));
             return acc
-        }, [] as number[]);
+        }, [] as number[])[0];
     }
 
-    function predict(dataArray: number[], daysNumber: number): number[] {
-        const predictionSampleSize = 3;
-        const accum = _.take(dataArray, predictionSampleSize)
-        console.warn('xxx: init - ', accum)
-        for (let i: number = predictionSampleSize; i < dataArray.length; i++) {
-            let logs = false;
-            // let logs = i === 9;
-            const dataArrayLastSample = _.slice(dataArray, i, +i + +predictionSampleSize + +1);
-            const prediction = buildPrediction(dataArrayLastSample, 1, dataArrayLastSample.length, logs)
-            let value = _.last(prediction)!!;
-            accum[i] = value
-            if (logs) {
-                console.warn('xxx: day ', labels[i].toISOString())
-                console.warn('xxx:', dataArrayLastSample)
-                console.warn('xxx:', value)
-            }
+
+    function nextDayPrediction(allDays: Dictionary<number>, currentDate: string, logs: boolean = false) {
+        const index = _.indexOf(_.keys(allDays), currentDate)
+        const lastPart = _.slice(_.values(allDays), index - 3, index + 1);
+        const prediction = buildPrediction(lastPart, logs)
+        if (logs) {
+            console.warn('xxx allDays:', allDays);
+            console.warn('xxx currentDate:', currentDate)
+            console.warn('xxx index:', index)
+            console.warn('xxx last:', lastPart)
+            console.warn('xxx last:', prediction)
         }
-        return accum
+        return prediction
     }
-
-    // console.warn('yyy')
-    // const testSet = [0, 0, 0, 2]
-    // const prediction = buildPrediction(testSet, 1, testSet.length, true)
-    // console.warn('xxx:', testSet)
-    // console.warn('xxx:', prediction)
 
     const buildData = () => {
-        const existingData = _.zip(labels.map(x => moment(x).format('l')), dataPerCountry)
-        const prediction = predict(existingData, days)
-        console.warn('xxx - existingData:', existingData)
-        console.warn('xxx - prediction:', prediction)
-        const existingDataNormalized = _.concat(existingData, _.fill(Array(prediction.length - existingData.length), NaN));
-        const predictedDataNormalized = _.concat(_.fill(Array(existingData.length), NaN), prediction);
-        const labelsNormalized = _.concat(labels, buildNextDaysLabels(_.last(labels), prediction.length)).map(date => moment(date).format('ll'));
-        let indexOfFirstPacient = _.findIndex(existingDataNormalized, (x) => x > 0) - 1
+        const existingData = _.zipObject(labels.map(x => moment(x).format('l')), dataPerCountry.map(Number))
+        const prediction = _.concat([0], _.map(_.keys(existingData), (date: string) => nextDayPrediction(existingData, date)))
+        // console.warn(nextDayPrediction(existingData, '1/02/2020', true))
+        const labelsNormalized = _.concat(labels, buildNextDaysLabels(_.last(labels)!, prediction.length - _.values(existingData).length)).map(date => moment(date).format('ll'));
+        let indexOfFirstPacient = _.findIndex(_.values(existingData), (x) => x > 0) - 1
         indexOfFirstPacient = indexOfFirstPacient < 0 ? 0 : indexOfFirstPacient
         return {
             labels: labelsNormalized.slice(indexOfFirstPacient),
             datasets: [
                 {
-                    ...pastLineStyle,
-                    label: country,
-                    data: existingDataNormalized.slice(indexOfFirstPacient)
-                },
-                {
                     ...predictionLineStyle,
                     label: country + ' (predicted)',
-                    data: predictedDataNormalized.slice(indexOfFirstPacient)
+                    data: _.values(prediction).slice(indexOfFirstPacient)
+                },
+                {
+                    ...pastLineStyle,
+                    label: country,
+                    data: _.values(existingData).slice(indexOfFirstPacient)
                 }
             ]
         }
