be60d11361a98a8bfa074c36e52f3cc628e0c083
p_val <palval16@gmail.com>
2020-03-22 12:28:59 +0100

    Better positioning for Dashboard.tsx content of iframe

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function Dashboard() {
+    useEffect(() => {
+        window.scrollTo(0, document.body.scrollHeight)
+        return () => {
+            window.scrollTo(0, 0)
+        }
+    }, [])
     return <>
-        <iframe title={"Dashboard"}
-                src="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
-                style={{width: '100%', height: '100vh', border: 0}}/>
+        <div style={{background: '#000000'}}>
+            <iframe title={"Dashboard"}
+                    src="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
+                    style={{width: '100%', height: '100vh', border: 0}}/>
+        </div>
     </>
 }
 
